81a08b4759e121967fd0cc836c15b5039e2a9544
McBaque <mcbaque01@gmail.com>
2024-06-04 20:58:09 +0800

    Commiting New Project!

---
+++ Backend/typescript/index.ts
@@ -0,0 +1,10 @@
+import app from './app/app.js'
+
+const server = app.listen(3000, async () =>{
+    console.log(`Server is working!`);
+    console.log(`Server running at PORT 3000`);
+})
+
+server.on('error', (err)=> {
+    console.log("Server Error: ", err)
+})
